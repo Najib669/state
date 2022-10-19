7e7b37d0be88ee508360012d4262a2dded39db23
najib <laflare@hotmail.com>
2022-10-19 10:38:21 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import pic from './studio.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person:{
+        fullName: "Najib Bohli",
+        bio: "bio ",        
+        profession: "Music Producer",
+        imgsrc:<img src={pic} alt='IMG' width={300}></img> ,     
+
+
+      },        
+      show: true,
+      counter:0 ,      
+    
+    }; 
+  }
+  handleClick = () => {
+    this.setState({show: !this.state.show,
+    });
+  };
+  componentDidMount() {
+    setInterval (() => {
+      this.setState ({ counter:this.state.counter+1});
+  },2);
+  }
+
+  render() {
+    return (
+      <div className='parent'>
+        {this.state.show ?
+                <div>
+                <h1>{this.state.person.fullName}</h1> 
+                <h1>{this.state.person.bio}</h1>        
+                <h1>{this.state.person.profession}</h1>
+                <h1>{this.state.person.imgsrc}</h1>
+                </div>
+        : null}
+
+        <button  onClick={this.handleClick} >
+        {this.state.show ? "hide profile" : " show profile "}
+        </button>
+      
+      </div> 
+    )
+
+  }
+  
 }
 
-export default App;
